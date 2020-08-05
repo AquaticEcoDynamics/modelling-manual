0e8f426f39e3f32cdee68ffb5de7f64156f5a543
gilesnknight <62376886+gilesnknight@users.noreply.github.com>
2020-08-05 09:27:20 +0800

    Updated

---
+++ resources/module2/Module 2 Resources/R/Plotting script.R
@@ -0,0 +1,10 @@
+#
+#Make a .png
+png(filename="Oxidants.png", width=160,height=120,res=500,units="mm")
+#Plot the variables
+plot(Time,O2/O2[1],col="red",ylab="Relative oxidant concentration")
+points(Time,NO3/NO3[1],col="blue")
+points(Time,Mn/Mn[1],col="green")
+points(Time,OM/OM[1],col="purple")
+#Close the .png 
+dev.off()
